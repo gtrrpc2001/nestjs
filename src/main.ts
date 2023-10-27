fb67b4d65e87208442acb4f37ee4151eaf9ee13e
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-10-27 18:14:14 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './module/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
