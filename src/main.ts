896b539ae667fdcaca052bf9cd6d8973bd631590
gtrrpc2001 <cbdgjs3548@gmail.com>
2025-01-08 17:36:58 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './module/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,8 +27,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors({
-    origin: [process.env.COR_ORIGIN_DOMAIN1, process.env.COR_ORIGIN_DOMAIN2, process.env.COR_ORIGIN_DOMAIN3, process.env.COR_ORIGIN_DOMAIN4],
-    // '*',    
+    origin: '*',
+    //
+    // [process.env.COR_ORIGIN_DOMAIN1, process.env.COR_ORIGIN_DOMAIN2, process.env.COR_ORIGIN_DOMAIN3, process.env.COR_ORIGIN_DOMAIN4],
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
     credentials: true
   });
